4c494247222ff493519f26cad31e7d95b4c62791
Trang2011 <trangtran.24hdev@gmail.com>
2020-02-26 16:16:24 +0700

    Setting up our Serverless infrastructure

--- handler/create.js
+++ handler/create.js
@@ -20,7 +20,7 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log(e);
+    console.log("error------------------------",e);
     return failure({ status: false });
   }
 }
